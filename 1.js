0dd9b18d97f31d9c0723a38a2a7498045743f5a7
mrisD <2173554564@qq.com>
2025-06-03 15:13:48 +0800

    增加搜索功能修复歌词接口错误bug

--- 1.js
+++ 1.js
@@ -40,18 +40,13 @@ function asrsea(text, pubKey, modulus, nonce = "0CoJUm6Qyw8W8jud") {
 }
 
 
-function ck(id){
+function ck(ik){
     // 示例调用
-const i2x = '{"ids":"[1394167216]","level":"exhigh","encodeType":"aac","csrf_token":"f8ca6f7e8ab34d79642bef1fbbdfacc4"}';
-
+const i2x = ik;
 return  asrsea(i2x, '010001', '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7');
 
 }
-// 示例调用
-const i2x = {
-    "logs": "[{\"action\":\"mobile_monitor\",\"json\":{\"meta._ver\":2,\"meta._dataName\":\"pip_lyric_monitor\",\"action\":\"render\",\"userAgent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0\",\"chromeVersion\":136,\"resourceId\":1394167216,\"resourceType\":\"song\",\"mainsite\":\"1\"}}]",
-    "csrf_token": "f8ca6f7e8ab34d79642bef1fbbdfacc4"
-};
+
 const i1='{"ids":"[1394167216]","level":"exhigh","encodeType":"aac","csrf_token":"f8ca6f7e8ab34d79642bef1fbbdfacc4"}'
 const result = asrsea(i1, '010001', '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7');
 
--- cs.py
+++ cs.py
@@ -4,19 +4,44 @@ import urllib.parse
 node = execjs.get('Node')  # 获取Node运行时
 with open('1.js', 'r', encoding='utf-8') as f:
     js_code = f.read()
-
+songname=input('输入歌曲名:')
 ctx = node.compile(js_code)
-result = ctx.call('ck', '2695944873')
-print(result)
+ik=f'{{"s":"{songname}","limit":"8","csrf_token":"f8ca6f7e8ab34d79642bef1fbbdfacc4"}}'
+result = ctx.call('ck', ik)
+# print(result)
+
 headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
 }
 
-url="https://music.163.com/weapi/song/enhance/player/url/v1?csrf_token=f8ca6f7e8ab34d79642bef1fbbdfacc4"
+url="https://music.163.com/weapi/search/suggest/web?csrf_token=f8ca6f7e8ab34d79642bef1fbbdfacc4"
 payload = (
     f'params={urllib.parse.quote(result["params"])}'
     f'&encSecKey={urllib.parse.quote(result["encSecKey"])}'
 )
-print(payload)
+# print(payload)
 response = requests.request("POST", url, headers=headers, data=payload)
-print(response.content)
+# print(response.json())
+songlist = response.json()["result"]["songs"]
+length = len(songlist)
+a=1
+for song in songlist:
+    print(a,song["name"],song["artists"][0]["name"])
+    a+=1
+songid=songlist[int(input(f"请输入歌曲前序号1-{length}:"))-1]["id"]
+# print(songid)
+#######################################################################
+ik2=f'{{"id":"{songid}","lv":-1,"tv":-1,"csrf_token":"f8ca6f7e8ab34d79642bef1fbbdfacc4"}}'
+result2 = ctx.call('ck', ik2)
+headers = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+}
+
+url="https://music.163.com/weapi/song/lyric?csrf_token=f8ca6f7e8ab34d79642bef1fbbdfacc4"
+payload = (
+    f'params={urllib.parse.quote(result2["params"])}'
+    f'&encSecKey={urllib.parse.quote(result2["encSecKey"])}'
+)
+# print(payload)
+response = requests.request("POST", url, headers=headers, data=payload)
+print(response.text)
